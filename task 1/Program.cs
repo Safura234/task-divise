03d251db56dcedb67821fa9f875078abc75cb686
Safura234 <safuramg@code.edu.az>
2022-03-13 21:00:26 +0400

    add task1

---
+++ task 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            string b = Console.ReadLine();
+           double num2 = Convert.ToDouble(b);
+            Divise(num1, num2);
+            Console.WriteLine(Divise(num1,num2));
+            Vurma(num1, num2);
+            Console.WriteLine(Vurma(num1, num2)); ;
+
+        }
+        static double Divise(double num1, double num2)
+        {
+            return num1 / num2; 
+        }
+        static double Vurma(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+           
+    }
+}
